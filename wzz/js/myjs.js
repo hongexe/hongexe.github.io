4fbf55649b053d6127c2169acd3a8d3bf26cd501
hongexe123 <1043816878@qq.com>
2014-05-13 21:03:15 +0800

    22
    22

--- wzz/js/myjs.js
+++ wzz/js/myjs.js
@@ -14,41 +14,18 @@ $$('div').tap(function() {
     // affects "span" children/grandchildren
     $$(this).style('color', 'red');
 });*/ 
-$(function(){  
-var imglist =document.getElementsByTagName("img");  
-//安卓4.0+等高版本不支持window.screen.width，安卓2.3.3系统支持  
-var _width;  
-doDraw();  
-  
-window.onresize = function(){  
-    //捕捉屏幕窗口变化，始终保证图片根据屏幕宽度合理显示  
-    doDraw();  
-}  
-  
-function doDraw(){  
-    _width = window.innerWidth;  
-    for( var i = 0, len = imglist.length; i < len; i++){  
-        DrawImage(imglist[i],_width);  
-    }  
-}  
-  
-function DrawImage(ImgD,_width){   
-    var image=new Image();   
-    image.src=ImgD.src;   
-    image.onload = function(){  
-        //限制，只对宽高都大于30的图片做显示处理  
-        if(image.width>30 && image.height>30){   
-            if(image.width>_width){  
-                ImgD.width=_width;   
-                ImgD.height=(image.height*_width)/image.width;   
+	 window.onresize = window.onload = function(){
+					var image =document.getElementsByTagName("img"); 
+					 if(image.width>30 && image.height>30){   
+            if(image.width>window.innerWidth){  
+                image.width=window.innerWidth;   
+                image.height=(388*window.innerWidth)/575;   
             }else{   
-                ImgD.width=image.width;   
-                ImgD.height=image.height;   
+                image.width=575;   
+                image.height=388;   
             }   
   
-        }     
-    }  
-  
-}  
-     
-}) 
+					 }
+			}	
+
+
