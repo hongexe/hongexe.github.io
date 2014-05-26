054c8e5f9a42ec0cf3e30e08b31055a1995cdaa7
hongexe123 <1043816878@qq.com>
2014-05-26 21:35:09 +0800

    2
    2

--- wy/js/custom.js
+++ wy/js/custom.js
@@ -29,11 +29,11 @@ $('.lbclose').click(function(){
 	if (expanded) {
 		$('.layerbottom').animate({bottom:'0'},700);
 		$('.layerbottom').animate({bottom:'-20'},600);
-		$(".updown").rotate({duration: 1000,angle: 0,animateTo: 540});
+		$(".updown").rotate({duration: 2000,angle: 0,animateTo: 540});
 
 	}else {
 		$('.layerbottom').animate({bottom:'-666'},400);
-		$(".updown").rotate({duration: 1000,angle: 540,animateTo: 1080});
+		$(".updown").rotate({duration: 2000,angle: 540,animateTo: 1080});
 	}
 	expanded = !expanded;
 });
