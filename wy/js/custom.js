08214adb2af8e3fd3f3ac850ae4350b33b6542ea
hongexe123 <1043816878@qq.com>
2014-05-26 16:57:07 +0800

    2
    2

--- wy/js/custom.js
+++ wy/js/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 			$('#wlpage').fullpage({
 				verticalCentered: true,
 				scrollingSpeed: 600,
+				 easing: 'linear',
 			});
 		});
 		
@@ -14,18 +15,18 @@ $('.lbclose').css('top','5px');
 var expanded = true;
 $('.lbtitle').click(function(){
 	if (expanded) {
-		$('.layerbottom').animate({bottom:'0'},200);
+		$('.layerbottom').animate({bottom:'0'},400);
 		$('.layerbottom').animate({bottom:'-20'},600);
 		$('.lbclose').animate({top:'-52'},700);
 
 	}else {
-		$('.layerbottom').animate({bottom:'-666'},200);
+		$('.layerbottom').animate({bottom:'-666'},400);
 		$('.lbclose').animate({top:'5'},500);
 	}
 	expanded = !expanded;
 });
 $('.lbclose').click(function(){
-		$('.layerbottom').animate({bottom:'-666'},300);
+		$('.layerbottom').animate({bottom:'-666'},400);
 		$('.lbclose').animate({top:'5'},500);
 });
 
