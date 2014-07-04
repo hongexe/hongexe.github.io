0814eec0d6c3e5c8039a1566705c488ddb70665e
hongexe123 <1043816878@qq.com>
2014-07-04 19:48:56 +0800

    2
    2

--- wy/js/index.js
+++ wy/js/index.js
@@ -25,30 +25,7 @@ $(document).ready(function() {
 				
 				}
         }
-		/*afterLoad: function(anchorLink, index){
-            if(index == '1'){
-				$(".gotoup").fadeOut();
-				$(".gotodown").fadeIn();
-				
-            } else if(index == '2'){
-				$(".gotoup").fadeIn();
-				$(".gotodown").fadeIn();
-				
-				} else if(index == '3'){
-				$(".gotoup").fadeIn();
-				$(".gotodown").fadeIn();
-				
-				} else if(index == '4'){
-				$(".gotoup").fadeIn();
-				$(".gotodown").fadeIn();
-				
-				} else if(index == '5'){
-				$(".gotoup").fadeIn();
-				$(".gotodown").fadeOut();
-				
-				}
-        }
-		*/
+		
 	});
 	
 });
