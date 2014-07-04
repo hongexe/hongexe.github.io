0846e6b408fbe351a2baa3d56d6047e0a3fcda66
hongexe123 <1043816878@qq.com>
2014-07-04 19:46:35 +0800

    2
    2

--- wy/js/index.js
+++ wy/js/index.js
@@ -2,6 +2,29 @@
 $(document).ready(function() {
 		$('#fullPage').fullpage({
 		easing: 'swing',
+		afterLoad: function(anchorLink, index){
+            if(index == '1'){
+				$(".gotoup").fadeOut();
+				$(".gotodown").fadeIn();
+				
+            } else if(index == '2'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeIn();
+				
+				} else if(index == '3'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeIn();
+				
+				} else if(index == '4'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeIn();
+				
+				} else if(index == '5'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeOut();
+				
+				}
+        }
 		/*afterLoad: function(anchorLink, index){
             if(index == '1'){
 				$(".gotoup").fadeOut();
