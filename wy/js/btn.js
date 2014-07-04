0cc34d034f50fad78fe7db098b685546fa63958b
hongexe123 <1043816878@qq.com>
2014-07-04 22:44:44 +0800

    2
    2

--- wy/js/btn.js
+++ wy/js/btn.js
@@ -1,8 +1,9 @@
 // JavaScript Document
-var shareTitle = '鸡哥一出，谁敢不从！《天下HD》7月开测召集令！即刻登录官网，抢先预约，争做大荒第一人！和我们一同开启小短腿们的世界传奇吧！';
+var shareTitle = '《天下HD》官方网站7月开测召集令！';
 var shareTxt = '鸡哥一出，谁敢不从！《天下HD》7月开测召集令！即刻登录官网，抢先预约，争做大荒第一人！和我们一同开启小短腿们的世界传奇吧！';
 var shareUrl = 'http://txhd.163.com';
 var sharePic = 'http://res.nie.netease.com/txhd/gw/14v2/pc/images/fenxiang.jpg';
+var shareIco = 'http://res.nie.netease.com/txhd/gw/14v2/pc/images/fenxiang.jpg';
 var winObj = $(window);
 
 $(document).ready(function() {
@@ -10,23 +11,21 @@ $(document).ready(function() {
 		easing: 'swing',
 		afterLoad: function(anchorLink, index){
             if(index == '1'){
-				$(".gotoup").fadeOut();
 				$(".gotodown").fadeIn();
 				
             } else if(index == '2'){
-				$(".gotoup").fadeIn();
 				$(".gotodown").fadeIn();
 				
 				} else if(index == '3'){
-				$(".gotoup").fadeIn();
+				
 				$(".gotodown").fadeIn();
 				
 				} else if(index == '4'){
-				$(".gotoup").fadeIn();
+				
 				$(".gotodown").fadeIn();
 				
 				} else if(index == '5'){
-				$(".gotoup").fadeIn();
+				
 				$(".gotodown").fadeOut();
 				
 				}
@@ -36,6 +35,12 @@ $(document).ready(function() {
 	
 });
 
+$('.sbtn').click(function () {
+        $('.share').fadeIn();
+});
+$('.gb').click(function () {
+        $('.share').fadeOut();
+});
 
     function validPhone(num) {
         if (/^(13|14|15|18)\d{9}$/.test(num)) {
@@ -50,8 +55,10 @@ $(document).ready(function() {
 
         if (!validPhone(pnum)) {
             alert("手机号码有误，请填写正确！");
+			$("#phone").focus();
         } else {
             collectPho("txhd", pnum, window.location.href);
+			$("#phone").focus();
         }
     });
 
@@ -82,6 +89,7 @@ $(document).ready(function() {
         var yzm = $("#yzm").val();
 
         collectCode("txhd", pnum, yzm);
+		$("#yzm").focus();
     });
 
     function collectCode(game_name, phone, authcode) {
@@ -103,14 +111,51 @@ $(document).ready(function() {
         });
     }
 	
+
 	
+	$(".yx").click(function(){
+		var _uri = "http://open.yixin.im/share?appkey=yx3ae08a776bf04178a583cb745fb6aa0c&type=webpage&url="+encodeURIComponent(shareUrl)+'&title='+encodeURIComponent(shareTitle)+'&desc='+encodeURIComponent(shareTxt)+'&pic='+encodeURIComponent(sharePic);
+		window.location.href=_uri;
+	});
+	$(".wb").click(function(){
+		var _uri="http://service.weibo.com/share/share.php?c=nie&content=gb2312&source=nie&title=" + encodeURIComponent(shareTitle) + "&url=" + encodeURIComponent(shareUrl) + "&pic=" + encodeURIComponent(sharePic);
+		window.location.href=_uri;
+	});
+	$(".kj").click(function(){
+		var _uri="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?summary=网易游戏&url=" + encodeURIComponent(shareUrl) + "&title=" + encodeURIComponent(shareTitle)+ "&desc=" + encodeURIComponent(shareTxt)+ "&pics=" + encodeURIComponent(sharePic);
+		window.location.href=_uri;
+	});
+	$(".py").click(function(){
+		var _uri = "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?to=pengyou&url=" + encodeURIComponent(shareUrl) + "&title=" + encodeURIComponent(shareTitle)+  "&desc=" + encodeURIComponent(shareTxt)+ "&pics=" + encodeURIComponent(sharePic);
+		window.location.href=_uri;
+	});
+	$('.wx').click(function () {
+       WeiXinShareBtn()
+});
+
+
+function WeiXinShareBtn() { 
+ if (typeof WeixinJSBridge == "undefined") { 
+ alert("请用微信打开！"); 
+ } else { 
+ WeixinJSBridge.invoke('shareTimeline', { 
+ "title": shareTitle, 
+ "link": shareUrl, 
+ "desc": shareTxt, 
+ "img_url": shareIco 
+ }); 
+ } 
+ }
+
+
+
 	
 //分享到微信朋友圈，微信朋友
 var onBridgeReady = function () {
 	var appId = '';
 	// 发送给好友;
 	WeixinJSBridge.on('menu:share:appmessage', function(argv){
-		var imgUrl = sharePic;
+		var imgUrl = shareIco;
 		var link = shareUrl;
 		var title = shareTitle;
 		var shareDesc = shareTxt;
@@ -127,10 +172,10 @@ var onBridgeReady = function () {
 	});
 	// 分享到朋友圈;
 	WeixinJSBridge.on('menu:share:timeline', function(argv){
-		var imgUrl = sharePic;
+		var imgUrl = shareIco;
 		var link = shareUrl;
 		var title = shareTitle;
-		var shareDesc = shareTxt2;
+		var shareDesc = shareTxt;
 		WeixinJSBridge.invoke('shareTimeline',{
 		'img_url' : imgUrl,
 		'img_width' : '640',
