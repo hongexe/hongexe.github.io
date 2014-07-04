bff1f85c38d63bd8d5a3ab7977f4abb7b3f5cd1a
hongexe123 <1043816878@qq.com>
2014-07-04 19:25:38 +0800

    2
    2

--- wy/js/index.js
+++ wy/js/index.js
@@ -1,1 +1,31 @@
 // JavaScript Document
+$(document).ready(function() {
+		$('#fullpage').fullpage({
+		easing: 'swing',
+		afterLoad: function(anchorLink, index){
+            if(index == '1'){
+				$(".gotoup").fadeOut();
+				$(".gotodown").fadeIn();
+				
+            } else if(index == '2'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeIn();
+				
+				} else if(index == '3'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeIn();
+				
+				} else if(index == '4'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeIn();
+				
+				} else if(index == '5'){
+				$(".gotoup").fadeIn();
+				$(".gotodown").fadeOut();
+				
+				}
+        }
+		
+	});
+	
+});
